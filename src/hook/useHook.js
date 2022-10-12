07d6aab3a0447310b42bd4f3e31a0b064948318a
GodofredoNahBurgos <skate_forever_1990@hotmail.com>
2022-10-11 19:23:50 -0500

    Hook useHook

---
+++ src/hook/useHook.js
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import { listArray } from "./services/listArray";
+
+export const useHook = () => {
+
+    const [list, setList] = useState(listArray);
+    const [inputValue, setInputValue] = useState({ name: "", value: "" });
+    const [error, setError] = useState("");
+
+  const handleOnchange = (event) => {
+    const { name, value } = event.target;
+    setInputValue((prev) => ({
+      ...prev,
+      name,
+      value
+    }));
+  };
+
+  const handleDelete = (id) => {
+    setList((prev) => ([
+      ...prev.filter((item) => item.id !== id)
+    ]));
+  };
+
+  const handleInsert = () => {
+    setList((prev) => ([
+      {
+        id: Math.floor(Math.random() * 100),
+        name: inputValue.value,
+      },
+      ...prev
+    ]));
+  };
+
+  const handleEdit = (id) => {
+    setList((prev) => ([
+      ...prev.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            name: inputValue.value
+          }
+        }
+        return item;
+      })
+    ]));
+  };
+
+  useEffect(() => {
+    if (inputValue.value === "") {
+      setError("Please enter a value");
+      console.log('ERR');
+    }else if (inputValue.value < 0 || inputValue.value > 0 ){
+      setError("Please enter a only letters values");
+      console.log('ERR');
+    }else {
+      setError("");
+    }
+  }, [inputValue.value]);
+
+  return {
+    handleDelete,
+    handleEdit,
+    handleInsert,
+    handleOnchange,
+    list
+  }
+
+}
